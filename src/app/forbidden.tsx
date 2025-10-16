85555bbc22fa4857a17c06618596de559f85da57
Dhanush R <dhanuraj7258@gmail.com>
2025-10-16 16:46:18 +0530

    feat(ui): add badge component and enhance forbidden page

--- src/app/forbidden.tsx
+++ src/app/forbidden.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@/components/ui/button';
+import { BanIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export default function ForbiddenPage() {
   return (
     <main className="flex grow items-center justify-center px-4 text-center">
       <div className="space-y-6">
-        <div className="space-y-2">
+        <div className="flex items-center flex-col space-y-2">
+          <BanIcon size="size-12 text-destructive" />
           <h1 className="text-2xl font-semibold">403 - Forbidden</h1>
           <p className="text-muted-foreground">
             You don&apos;t have access to this page.
